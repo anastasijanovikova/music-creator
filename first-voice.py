dde93aa27f7e2d5d4e0abe5d7d15e1a0142a0710
Anastasija Novikova <nastja.new@gmail.com>
2017-04-28 23:39:48 +0200

    Update first-voice.py

--- first-voice.py
+++ first-voice.py
@@ -1,4 +1,15 @@
 import music21
 import string
 
-print("Enter your birth date")
+print("Enter your birth date and name")
+day_in = int(input())
+month_in = int(input())
+year_in = int(input())
+name = raw_input()
+name = name.lower()
+name_num = list()
+name = list(name)
+fname = raw_input()
+fname = fname.lower()
+fname_num = list()
+fname = list(fname)
